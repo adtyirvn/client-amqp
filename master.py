0fdfdb2a23ea376c750edde88ab7a4dbb975a907
Aditya Irvan <adityairvanku@gmail.com>
2023-05-24 16:43:49 +0700

    fix bug

--- master.py
+++ master.py
@@ -12,10 +12,7 @@ queue_name = os.getenv('QUEUE_NAME')
 
 async def main():
     receiver = amqp_controller.AMQPReceiver(rabbitmq_server, queue_name)
-    try:
-        await receiver.start()
-    except Exception as e:
-        print(e)
+    await receiver.start()
 try:
     asyncio.run(main())
 except KeyboardInterrupt:
--- src/amqp_controller.py
+++ src/amqp_controller.py
@@ -1,4 +1,3 @@
-import asyncio
 import aio_pika
 from . import ascon
 from . import config
@@ -18,41 +17,47 @@ class AMQPReceiver:
         self.consumer = None
 
     async def connect(self):
-            self.connection = await aio_pika.connect_robust(self.host, heartbeat=30)
-            self.channel = await self.connection.channel()
-            queue = await self.channel.declare_queue(self.queue_name, durable=False)
-            self.consumer = queue.iterator()
+        self.connection = await aio_pika.connect_robust(self.host, heartbeat=30)
+        self.channel = await self.connection.channel()
+        queue = await self.channel.declare_queue(self.queue_name, durable=False)
+        self.consumer = queue.iterator()
 
     async def start(self):
         try:
             await self.connect()
-
-            async for message in self.consumer:
-                try:
-                    await self.on_message(message)
-                except Exception as e:
-                    print(f"Error processing message: {e}\n")
-        
+            await self.consume_messages()
         except Exception as e:
-            raise Exception(f"Error start received message: {e}\n")
+           print(f"Error starting AMQP receiver: {e}\n")
+        finally:
+            await self.close_amqp()
 
-    async def on_message(self, message: aio_pika.IncomingMessage):
+    async def consume_messages(self):
+        async for message in self.consumer:
+            try:
+                await self.process_message(message)
+            except Exception as e:
+                print(f"Error processing message: {e}\n")
+
+    async def process_message(self, message: aio_pika.IncomingMessage):
+        async with message.process():
+            global nonce_g
+
+            def decryption(ascon, ciphertext, key, nonce, mode="ECB"):
+                plaintext = ascon.ascon_decrypt(
+                    key, nonce, associateddata=b"", ciphertext=ciphertext, variant="Ascon-128"
+                )
+                if mode == "CBC":
+                    new_nonce = ciphertext[:16]
+                    return plaintext, new_nonce
+
+            plaintext, nonce_g = decryption(asc, message.body, key_g, nonce_g, "CBC")
+            message_json = plaintext.decode("utf-8")
+            message_dict = json.loads(message_json)
+            print(f"*** Received message ***\n{message.body}\n{message_dict}\n")
+
+    async def close_amqp(self):
         try:
-            async with message.process():
-                global nonce_g
-
-                def decryption(ascon, ciphertext, key, nonce, mode="ECB"):
-                    plaintext = ascon.ascon_decrypt(
-                        key, nonce, associateddata=b"", ciphertext=ciphertext, variant="Ascon-128"
-                    )
-                    if mode == "CBC":
-                        new_nonce = ciphertext[:16]
-                        return plaintext, new_nonce
-
-                plaintext, nonce_g = decryption(asc, message.body, key_g, nonce_g, "CBC")
-                message_json = plaintext.decode("utf-8")
-                message_dict = json.loads(message_json)
-                print(f"*** Received message ***\n{message.body}\n{message_dict}\n")
-
+            if self.connection is not None:
+                await self.connection.close()
         except Exception as e:
-            raise RuntimeError(e)
+            print("Error closing AMQP connection:", e)
